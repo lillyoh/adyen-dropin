fccc2d6acbcece8169bfebc687ef70b7f8ce7c7a
Lilly Oh <ohlilly@gmail.com>
2021-08-24 11:31:45 -0700

    added dropin configuration

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+const { v4: uuidv4 } = require('uuid');
 const { Client, Config, CheckoutAPI } = require('@adyen/api-library');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Render HTML file into the server
+app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
 
 const config = new Config();
 config.apiKey = process.env.API_KEY;
@@ -10,16 +19,8 @@ const client = new Client({ config });
 client.setEnvironment('TEST');
 const checkout = new CheckoutAPI(client);
 
-
-// Render HTML file into the server
-app.use(express.static(__dirname));
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-})
-
-
 // Get available payment methods
-app.post('/', async (req, res) => {
+app.post('/api/getPaymentMethods', async (req, res) => {
   try {
     const response = await checkout.paymentMethods({
       merchantAccount: config.merchantAccount,
@@ -30,11 +31,37 @@ app.post('/', async (req, res) => {
       channel: 'Web',
       countryCode: 'US',
       shopperLocale: 'en-US'
-    })
+    });
     res.json(response);
   } catch (error) {
     console.error(`Error: ${error}`)
+  }
+})
 
+
+// Initiate a payment
+const paymentDataStore = {};
+app.post('/api/initiatePayment', async (req, res) => {
+  try {
+    const orderRef = uuidv4();
+    const response = await checkout.payments({
+      amount: { currency: 'USD', value: 1000 },
+      reference: orderRef,
+      merchantAccount: process.env.MERCHANT_ACCOUNT,
+      channel: 'Web',
+      returnUrl: `http://localhost:5000/api/handleShopperRedirect?orderRef=${orderRef}`,
+      browserInfo: req.body.browserInfo,
+      paymentMethod: req.body.paymentMethod
+    });
+
+    const { action } = response;
+
+    if (action) {
+      paymentDataStore[orderRef] = action.paymentData;
+    }
+    res.json(response);
+  } catch (error) {
+    console.error(`Error: ${error}`)
   }
 })
 
